a91b03e0b8ef379629041086c2f9c75ebb5fe2f9
melyymix1 <mgutierrezs2104@alumno.ipn.mx>
2023-12-01 04:51:39 -0600

    Eliminar firebase-config.js

--- Bachecito/firebase-config.js
+++
@@ -1,9 +0,0 @@
-
-export const firebaseConfig = {
-  apiKey: "AIzaSyBTmAonjcSsCcNwW7S1EQI80_vut8PIK5A",
-  authDomain: "bachecito-26-2.firebaseapp.com",
-  projectId: "bachecito-26-2",
-  storageBucket: "bachecito-26-2.appspot.com",
-  messagingSenderId: "445662169591",
-  appId: "1:445662169591:web:b169f2fdef3b1719db86f2"
-  };
